108aaade6b817a44ae52eb5d77db35a9036fa415
Harini G <93110552+harini293@users.noreply.github.com>
2021-12-12 12:15:37 +0000

    done

---
+++ p2original.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+int input()
+{
+	int a;
+	printf("enter any number :");
+	scanf("%d",&a);
+	return a;
+}
+
+int add(int a,int b)
+{
+	int c;
+	c=a+b;
+	return c;
+}
+
+int output(int a,int b,int c)
+{
+	printf("the sum of %d and %d is %d",a,b,c);
+
+}
+
+int main()
+{
+	int x,y,z,sum;
+	x=  input();
+	y= input();
+  z= add(x,y);
+  output(x,y,z);
+  return sum;
+}
