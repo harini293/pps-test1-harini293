b12b2fa32590de853990e77c094b2c549d9516be
Harini G <93110552+harini293@users.noreply.github.com>
2022-01-02 13:32:34 +0000

    finished

---
+++ p5final.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+void input(int *a,int *b,int *c);
+void cmp(int a,int b, int c,int *large);
+void output(int large);
+
+int main()
+{
+   int a,b,c,large;
+   input(&a,&b,&c);
+   cmp(a,b,c,&large);
+  output(large);
+}
+
+void input(int *a, int *b, int *c)
+{
+	printf("enter a number : ");
+	scanf("%d",a);
+	printf("enter a number : ");
+	scanf("%d",b);
+	printf("enter a number : ");
+	scanf("%d",c);
+}
+
+void cmp(int a,int b,int c,int *large)
+{
+	if(a>b && a>c)
+	*large=a;
+	else if(b>a)
+	*large=b;
+	else if(c>a)
+	*large=c;
+}
+
+void output(int large)
+{
+   printf("the larger number is %d",large);
+}
--- p5original.c
+++ p5original.c
@@ -2,13 +2,14 @@
 
 void input(int *a,int *b,int *c);
 void cmp(int a,int b, int c,int *large);
+void output(int large);
 
 int main()
 {
    int a,b,c,large;
    input(&a,&b,&c);
    cmp(a,b,c,&large);
-   printf("the larger number is %d",large);
+  output(large);
 }
 
 void input(int *a, int *b, int *c)
@@ -29,4 +30,9 @@ void cmp(int a,int b,int c,int *large)
 	*large=b;
 	else if(c>a)
 	*large=c;
+}
+
+void output(int large)
+{
+   printf("the larger number is %d",large);
 }
